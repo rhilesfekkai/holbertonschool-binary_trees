88d1c7f41299f70363a3cd319d57f82383fdbc58
rhilesfekkai <ramirhiles@gmail.com>
2026-04-06 17:40:42 +0000

    Ajout des fonctions binary_tree_insert_right et binary_tree_delete, résolution des conflits

---
+++ 3-main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	binary_tree_t *root;
+
+	root = binary_tree_node(NULL, 98);
+	root->left = binary_tree_node(root, 12);
+	root->right = binary_tree_node(root, 402);
+	binary_tree_insert_right(root->left, 54);
+	binary_tree_insert_right(root, 128);
+
+	/* Print the tree before deletion */
+	binary_tree_print(root);
+
+	/* Delete the entire tree */
+	binary_tree_delete(root);
+
+	return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -8,22 +8,24 @@
  * struct binary_tree_s - Node of a binary tree
  *
  * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child
- * @right: Pointer to the right child
+ * @parent: Pointer to parent node
+ * @left: Pointer to left child
+ * @right: Pointer to right child
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
-/* Prototypes */
+typedef struct binary_tree_s binary_tree_t;
+
+/* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
