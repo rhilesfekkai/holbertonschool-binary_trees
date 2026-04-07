65cbdf05123c95df713116b8c562b7d206a4c51f
rhilesfekkai <ramirhiles@gmail.com>
2026-04-07 10:41:43 +0000

    task8

--- main_0.c
+++ main_0.c
@@ -1,8 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * print_num - Affiche un entier
- * @n: Nombre à afficher
+ * print_num - Prints a number
+ * @n: Number to be printed
  */
 void print_num(int n)
 {
@@ -10,13 +12,15 @@ void print_num(int n)
 }
 
 /**
- * main - Test de binary_tree_preorder et binary_tree_delete
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
     binary_tree_t *root;
 
+    /* Création de l’arbre */
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
     root->right = binary_tree_node(root, 402);
@@ -25,10 +29,20 @@ int main(void)
     root->right->left = binary_tree_node(root->right, 256);
     root->right->right = binary_tree_node(root->right, 512);
 
+    /* Affichage de l’arbre */
+    binary_tree_print(root);
+
+    /* Traversals */
     printf("Pre-order traversal:\n");
     binary_tree_preorder(root, &print_num);
 
-    /* Libération de l'arbre */
+    printf("In-order traversal:\n");
+    binary_tree_inorder(root, &print_num);
+
+    printf("Post-order traversal:\n");
+    binary_tree_postorder(root, &print_num);
+
+    /* Nettoyage */
     binary_tree_delete(root);
 
     return (0);
