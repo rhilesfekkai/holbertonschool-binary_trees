281d6cf143eb2897f2f791cb0d08c0626b054d0d
rhilesfekkai <ramirhiles@gmail.com>
2026-04-07 09:26:38 +0000

    Task 6: binary_tree_preorder

---
+++ 6-main.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/* Fonction pour afficher un entier */
+void print_num(int n)
+{
+    printf("%d\n", n);
+}
+
+/* Test du préordre */
+int main(void)
+{
+    binary_tree_t *root;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    root->left->left = binary_tree_node(root->left, 6);
+    root->left->right = binary_tree_node(root->left, 56);
+    root->right->left = binary_tree_node(root->right, 256);
+    root->right->right = binary_tree_node(root->right, 512);
+
+    binary_tree_print(root);
+
+    printf("Pre-order traversal:\n");
+    binary_tree_preorder(root, &print_num);
+
+    return (0);
+}
