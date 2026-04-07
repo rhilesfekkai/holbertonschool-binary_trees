63985e6b47177c5cff6abe57c3491e0762e28d98
rhilesfekkai <ramirhiles@gmail.com>
2026-04-07 09:09:30 +0000

    Task 5: binary_tree_is_root

--- binary_trees.h
+++ binary_trees.h
@@ -1,22 +1,30 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/* Structure for binary tree node */
+/**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to parent
+ * @left: Pointer to left child
+ * @right: Pointer to right child
+ */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Prototypes */
+/* Functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *tree);
 
-#endif /* BINARY_TREES_H */
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+
+#endif
